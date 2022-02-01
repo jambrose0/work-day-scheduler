93365111d3c2b4e13c02141c83903565ea51325d
jambrose0 <jambrose0518@gmail.com>
2022-02-01 18:44:56 -0500

    made changes to css in body

--- assets/css/js/scipt.js
+++ assets/css/js/scipt.js
@@ -1,1 +1,11 @@
+//dom elements
 var timeBlocks = document.getElementById("time-blocks");
+
+var workDay = moment().startOf("day");
+
+for (var i = 0; i < workDay; i++) {
+  var toPrint = moment(workDay)
+    .add(1 + i, "hours")
+    .format(hhA);
+  console.log(toPrint);
+}
