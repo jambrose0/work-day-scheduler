111e16a847e236969065ed889acff0a25537e4a3
jambrose0 <jambrose0518@gmail.com>
2022-02-05 15:17:22 -0500

    changed javascript to be more jquery focused

--- assets/js/scipt.js
+++ assets/js/scipt.js
@@ -33,64 +33,16 @@ $("textarea").each(function (index) {
     $(this).addClass("future");
   }
   //get localStorage here
+  var loadTask = localStorage.getItem($(this).data("hour"));
+  $(this).text(loadTask);
 });
 
 var saveTasks = function (event) {
   event.preventDefault();
-  localStoreage.setItem($(this).$("textarea"));
+  localStorage.setItem(
+    $(this).siblings("h3").attr("id"),
+    $(this).siblings("textarea").val()
+  );
 };
 
-//dom elements
-// var workHours = document.getElementById("time-block");
-// var saves = document
-//   .getElementById("store-block")
-//   .addEventListener("click", keepTasks);
-
-// function taskChecker() {
-//   for (var i = 0; i <= workHours[index]; i++);
-//   if (workHours < displayTime) {
-//     tasks.classList.add("past");
-//   }
-//   if ((workHours = displayTime)) {
-//     tasks.classList.add("present");
-//   }
-//   if (workHours > displayTime) {
-//     tasks.classList.add("future");
-//   }
-// }
-
-// function handleUpdatingHours() {
-//   if (timeNow < workHours) {
-//     tasks.classList.add("past");
-//   }
-//   if ((timeNow = workHours)) {
-//     tasks.classList.add("present");
-//   }
-//   if (timeNow > workHours) {
-//     tasks.classList.add("future");
-//   }
-// }
-// use a for loop that iterates through each time value you have set as your time variables,
-//then it needs to compare each iteration of time with the current time from the previous variable
-
-//then the for loop can have a conditional that says what to do
-//if the time block is before the current time, do this...
-// else if it is equal to current time do this..
-// else if it is greater than current time, do this.
-
-//store text area function
-// function keepTasks() {
-//   var input = document.getElementById("task-block");
-//   localStorage.setItem("task-block", input);
-//   //locally store items from text area as needed
-//   //retrieve tasks upon returning to page
-// }
-// function showTasks() {
-//   var output = localStorage.getItem("task-block");
-// }
-
-//on click events
-
-// handleUpdatingHours();
-// showTasks();
-// taskChecker();
+saveBtn.on("click", saveTasks);
